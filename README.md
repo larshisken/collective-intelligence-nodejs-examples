242fa1008bda165a1e4559533b38d79e67f2dcc5
Lars Hisken <larshisken@protonmail.com>
2017-03-19 12:05:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Programming Collective Intelligence
+
+A collection of examples from [Programming Collective Intelligence](http://oreilly.com/catalog/9780596529321/) rewritten in Javascript
+
+## Reference
+- “Programming Collective Intelligence by Toby Segaran. Copyright 2007 Toby Segaran, 978-0-596-52932-1.”
---
+++ chapter02/Euclidean.js
@@ -0,0 +1,42 @@
+const critics = require('../recommendations');
+
+let Euclidean = class Euclidean {
+
+    // Returns a distance-based similarity score for person1 and person2
+    static simDistance(prefs, person1, person2) {
+
+        // Get the list of shared_items
+        let si = prefs[person1].filter(item => {
+            let m = prefs[person2].some(el => {
+                return el[Object.keys(item)[0]];
+            })
+            if (m) return true;
+        });
+
+        // If they have no ratings in common, return 0
+        if (si.length === 0) return 0;
+
+        // Add up the squares of all the differences
+        let sum = 0;
+        prefs[person1].forEach(item => {
+            let m = prefs[person2].some(el => {
+                return el[Object.keys(item)[0]]
+            });
+
+            if (m) {
+                let person2_pref = prefs[person2].find(el => {
+                    if (el[Object.keys(item)[0]]) return true;
+                })
+                let calc = Math.pow(item[Object.keys(item)[0]] - person2_pref[Object.keys(item)[0]], 2);
+                sum += calc;
+            }
+        });
+        return 1 / (1 + sum);
+
+    }
+
+}
+
+module.export = Euclidean;
+
+console.log(Euclidean.simDistance(critics, 'Lisa Rose', 'Gene Seymour'));
