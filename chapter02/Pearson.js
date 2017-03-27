3a89f1a08daca598a69e079e11e1691dd91a5a54
Lars Hisken <larshisken@protonmail.com>
2017-03-27 21:59:36 +0200

    Using lodash in Pearson script

--- chapter02/Pearson.js
+++ chapter02/Pearson.js
@@ -1,18 +1,21 @@
+const _ = require('lodash');
+
 module.exports = class Pearson {
 
     // Returns the Pearson correlation coefficient for p1 and p2
     static sim(prefs, person1, person2) {
 
-        // Get the list of mutually rated items
-        let si = prefs[person1].filter(item => {
-            let m = prefs[person2].some(el => {
-                return el[Object.keys(item)[0]];
-            })
-            if (m) return true;
+        // Get the list of shared_items
+        let person1Movies = _.map(prefs[person1], (n) => {
+            return _.keys(n)[0];
         });
+        let person2Movies = _.map(prefs[person2], (n) => {
+            return _.keys(n)[0];
+        });
+        let sharedMovies = _.intersection(person1Movies, person2Movies);
 
-        // If they are no ratings in common, return 0
-        if (si.length === 0) return 0;
+        // If they have no ratings in common, return 0
+        if (sharedMovies.length === 0) return 0;
 
         let sum1 = 0;
         let sum1Sq = 0;
@@ -20,28 +23,30 @@ module.exports = class Pearson {
         let sum2Sq = 0;
         let pSum = 0;
 
-        si.forEach(sip1 => {
+        _.forEach(sharedMovies, (value) => {
 
-            let key = Object.keys(sip1)[0];
-            let sip2 = prefs[person2].find(el => {
-                if (el[key]) return true;
-            });
+            let person1Rating = _.values(_.find(prefs[person1], (e) => {
+                if (e[value]) return e[value];
+            }))[0];
+            let person2Rating = _.values(_.find(prefs[person2], (e) => {
+                if (e[value]) return e[value];
+            }))[0];
 
             // Add up all the preferences
-            sum1 += sip1[key];
-            sum2 += sip2[key];
+            sum1 += person1Rating;
+            sum2 += person2Rating;
 
             // Sum up the squares
-            sum1Sq += Math.pow(sip1[key], 2);
-            sum2Sq += Math.pow(sip2[key], 2);
+            sum1Sq += Math.pow(person1Rating, 2);
+            sum2Sq += Math.pow(person2Rating, 2);
 
             // Sum up the products
-            pSum += sip1[key] * sip2[key];
+            pSum += person1Rating * person2Rating;
 
         });
 
-        let num = pSum - (sum1 * sum2 / si.length);
-        let den = Math.sqrt((sum1Sq - Math.pow(sum1, 2) / si.length) * (sum2Sq - Math.pow(sum2, 2) / si.length));
+        let num = pSum - (sum1 * sum2 / sharedMovies.length);
+        let den = Math.sqrt((sum1Sq - Math.pow(sum1, 2) / sharedMovies.length) * (sum2Sq - Math.pow(sum2, 2) / sharedMovies.length));
 
         if (den === 0) return 0;
 
